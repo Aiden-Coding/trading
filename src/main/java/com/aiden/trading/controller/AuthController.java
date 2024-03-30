28360926d5eb4da6d7941747cb3cd44daa2a7a65
dong <dwx_job@163.com>
2024-03-30 12:21:00 +0800

    1

---
+++ src/main/java/com/aiden/trading/controller/AuthController.java
@@ -0,0 +1,49 @@
+package com.aiden.trading.controller;
+
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@Tag(name = "认证")
+public class AuthController {
+    // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
+    @RequestMapping("doLogin")
+    public SaResult doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+
+            // 第2步，获取 Token  相关参数
+            SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+            return SaResult.data(tokenInfo);
+        }
+
+
+        return SaResult.error("登录失败");
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
+    @RequestMapping("isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
+    }
+
+    // 查询 Token 信息  ---- http://localhost:8081/acc/tokenInfo
+    @RequestMapping("tokenInfo")
+    public SaResult tokenInfo() {
+        return SaResult.data(StpUtil.getTokenInfo());
+    }
+
+    // 测试注销  ---- http://localhost:8081/acc/logout
+    @RequestMapping("logout")
+    public SaResult logout() {
+        StpUtil.logout();
+        return SaResult.ok();
+    }
+
+}
---
+++ src/main/java/com/aiden/trading/handler/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.aiden.trading.handler;
+
+import cn.dev33.satoken.util.SaResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    // 全局异常拦截
+    @ExceptionHandler
+    public SaResult handlerException(Exception e) {
+        log.error(e.getMessage(),e);
+        return SaResult.error(e.getMessage());
+    }
+}
