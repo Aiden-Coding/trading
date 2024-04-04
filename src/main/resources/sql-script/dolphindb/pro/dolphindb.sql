d546e206198f37a63eb5251b2fe28fb667ea912d
dong <dwx_job@163.com>
2024-04-04 17:10:19 +0800

    日线数据

--- src/main/resources/sql-script/dolphindb/pro/dolphindb.sql
+++ src/main/resources/sql-script/dolphindb/pro/dolphindb.sql
@@ -4,7 +4,7 @@ yearRange=date(1990.01M + 12*0..99)
 if(existsDatabase(dbPath)){
     dropDatabase(dbPath)
 }
-columns1=`code`trade_date`open`high`low`close`volume`amount
-type1=`SYMBOL`TIMESTAMP`DOUBLE`DOUBLE`DOUBLE`DOUBLE`DOUBLE`DOUBLE
+columns1=`code`time`open`high`low`close`volume
+type1=`SYMBOL`TIMESTAMP`DOUBLE`DOUBLE`DOUBLE`DOUBLE`DOUBLE
 db=database(dbPath,RANGE,yearRange,engine="TSDB")
-hushen_daily_line=db.createPartitionedTable(table(100000000:0,columns1,type1),`daily_line,`trade_date)
+hushen_daily_line=db.createPartitionedTable(keyedTable(`code`time,100000000:0,columns1,type1),`daily_line,`time,keepDuplicates=LAST,sortColumns=`code`time)
