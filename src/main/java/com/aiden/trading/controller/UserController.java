8c6ca590c6dee10f5287733a4c0e1d1a356f7e3f
dong <dwx_job@163.com>
2024-04-30 22:46:16 +0800

    日线数据

--- src/main/java/com/aiden/trading/controller/UserController.java
+++ src/main/java/com/aiden/trading/controller/UserController.java
@@ -1,34 +1,19 @@
 package com.aiden.trading.controller;
 
-import cn.dev33.satoken.stp.SaTokenInfo;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
 import com.aiden.trading.dto.Result;
-import com.aiden.trading.dto.user.req.LoginReq;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/user")
 @Tag(name = "认证")
 public class UserController {
-    // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
-    @PostMapping("doLogin")
-    public Result<?> doLogin(@RequestBody LoginReq loginReq) {
-        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
-        if("vben".equals(loginReq.getUsername()) && "123456".equals(loginReq.getPassword())) {
-            StpUtil.login(10001);
-            // 第2步，获取 Token  相关参数
-            SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
-            return Result.data(tokenInfo);
-        }
 
-
-        return Result.error("登录失败");
-    }
 
     // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
     @GetMapping("getUserInfo")
