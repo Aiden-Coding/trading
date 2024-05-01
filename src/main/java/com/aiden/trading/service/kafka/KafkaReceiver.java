3c94a45113b16797fc72104edae42e6fe1197764
dong <dwx_job@163.com>
2024-05-01 16:24:25 +0800

    日线数据

--- src/main/java/com/aiden/trading/service/kafka/KafkaReceiver.java
+++ src/main/java/com/aiden/trading/service/kafka/KafkaReceiver.java
@@ -15,10 +15,10 @@ public class KafkaReceiver {
      */
     @KafkaListener(topics = {"stock_subscribe"})
     public void receive(ConsumerRecord<?, ?> record, Acknowledgment ack){
-        log.info("消费者收到的消息key: " + record.key());
-        log.info("消费者收到的消息value: " + record.value().toString());
         try {
-            log.info("topic is {}, offset is  {},partition is {}, value is  {} ", record.topic(), record.offset(),record.partition(), record.value());
+            log.info("topic is {}, key is {}, offset is  {},partition is {}", record.topic(), record.key(),record.offset(),record.partition());
+            //"301203.SZ": {"time": 1714460700000, "lastPrice": 32.69, "open": 32.27, "high": 32.87, "low": 32.2, "lastClose": 32.39, "amount": 0.0, "volume": 0, "pvolume": 0, "stockStatus": 0, "openInt": 22, "transactionNum": 0, "lastSettlementPrice": 0.0, "settlementPrice": 0.0, "pe": 0.0, "askPrice": [0.0, 0.0, 0.0, 0.0, 0.0], "bidPrice": [0.0, 0.0, 0.0, 0.0, 0.0], "askVol": [0, 0, 0, 0, 0], "bidVol": [0, 0, 0, 0, 0], "volRatio": 0.0, "speed1Min": 0.0, "speed5Min": 0.0}
+
             // 手动提交offset
             ack.acknowledge();
         }catch (Exception e){
