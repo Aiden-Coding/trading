3d8b3f0a91bda645aa11f622342f595e6f8fe6d1
Aiden Dong <dwx_job@163.com>
2024-05-13 00:56:05 +0800

    1

--- src/main/java/com/aiden/trading/interceptor/WebSocketInterceptor.java
+++ src/main/java/com/aiden/trading/interceptor/WebSocketInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class WebSocketInterceptor implements HandshakeInterceptor {
@@ -16,6 +17,8 @@ public class WebSocketInterceptor implements HandshakeInterceptor {
         if (request instanceof ServletServerHttpRequest) {
             ServletServerHttpRequest servletServerHttpRequest = (ServletServerHttpRequest) request;
             // 模拟用户（通常利用JWT令牌解析用户信息）
+//            new String(servletServerHttpRequest.getServletRequest().getHeader("CustomHeader1").getBytes(StandardCharsets.ISO_8859_1),"UTF-8");
+//            String userId = servletServerHttpRequest.getServletRequest().getParameter("uid");
             String userId = servletServerHttpRequest.getServletRequest().getParameter("uid");
             // TODO 判断用户是否存在
             attributes.put("uid", userId);
--- src/main/java/com/aiden/trading/service/impl/WebSocketImpl.java
+++ src/main/java/com/aiden/trading/service/impl/WebSocketImpl.java
@@ -1,12 +1,16 @@
 package com.aiden.trading.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.aiden.trading.dto.MessagePackage;
 import com.aiden.trading.service.WebSocket;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.utils.CopyOnWriteMap;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
@@ -22,7 +26,11 @@ public class WebSocketImpl implements WebSocket {
     /**
      * 线程安全的无序集合（存储会话）
      */
+    /**
+     * 线程安全的无序集合（存储会话）
+     */
     private final CopyOnWriteArraySet<WebSocketSession> sessions = new CopyOnWriteArraySet<>();
+    private final CopyOnWriteMap<String,String> sessionEev = new CopyOnWriteMap<>();
 
     @Override
     public void handleOpen(WebSocketSession session) {
@@ -34,6 +42,7 @@ public class WebSocketImpl implements WebSocket {
     @Override
     public void handleClose(WebSocketSession session) {
         sessions.remove(session);
+        sessionEev.remove(session.getId());
         int count = connectionCount.decrementAndGet();
         log.info("a new connection closed，current online count：{}", count);
     }
@@ -42,6 +51,15 @@ public class WebSocketImpl implements WebSocket {
     public void handleMessage(WebSocketSession session, String message) {
         // 只处理前端传来的文本消息，并且直接丢弃了客户端传来的消息
         log.info("received a message：{}", message);
+//        try {
+//            MessagePackage messagePackage = JSONUtil.toBean(message, MessagePackage.class);
+//            if (Objects.equals("login",messagePackage.getCode())) {
+//                sessionEev.put(session.getId(),messagePackage.getMsg());
+//            }
+//            this.sendMessage(session, message);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
     @Override
